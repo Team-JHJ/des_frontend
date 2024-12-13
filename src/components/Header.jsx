c143fed4c3bdcc3dc536ed854a68c14414bc7b1c
khanna01 <govmfkdtm@gmail.com>
2024-12-13 21:48:42 +0900

    [퍼블리싱] 헤더

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,45 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-regular-svg-icons'
+import { useEffect, useState } from 'react'
+
+const TimeDisplay = () => {
+    const [date, setDate] = useState(new Date())
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+
+        return () => clearInterval(interval)
+    }, [])
+
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    const hour = date.getHours()
+    const minute = String(date.getMinutes()).padStart(2, '0')
+    const second = String(date.getSeconds()).padStart(2, '0')
+
+    return (
+        <div className="absolute top-5 right-8 text-[#797979]">
+            <FontAwesomeIcon icon={faClock} />
+            {` ${year}년 ${month}월 ${day}일 ${hour}:${minute}:${second}`}
+        </div>
+    )
+}
+
 export default function Header() {
     return (
         <div className="h-28 bg-[url('@/assets/img/header_bg.png')] bg-no-repeat bg-cover">
-            <div className="container mx-auto h-full px-10 py-5">
-                <p>제목</p>
+            <div className="container mx-auto h-full px-10 py-6 relative">
+                <p className="text-3xl font-medium">제목</p>
+                {/* 시간 */}
+                <TimeDisplay />
+                {/* 헤더 관리 */}
+                <div className="absolute right-8 bottom-4">
+                    <button></button>
+                    <button></button>
+                </div>
             </div>
         </div>
     )
